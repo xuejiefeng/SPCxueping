3aeb72c114fe8ef063b68ebc0a3c68783a268d2e
liuzijian <1251052604@qq.com>
2021-07-13 20:27:20 +0800

    first commit

---
+++ src/test/java/com/personal/spcxueping/SpCxuepingApplicationTests.java
@@ -0,0 +1,14 @@
+package com.personal.spcxueping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpCxuepingApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("123456");
+    }
+
+}
